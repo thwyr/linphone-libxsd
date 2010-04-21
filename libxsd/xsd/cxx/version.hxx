9bee16eff6e972a69093ae8e854619986c315581
Boris Kolpackov <boris@codesynthesis.com>
2010-04-22 00:02:46 +0200

    Bump version to 3.3.0.b3

--- libxsd/xsd/cxx/version.hxx
+++ libxsd/xsd/cxx/version.hxx
@@ -23,7 +23,7 @@
 // 3.0.0.b2    02999952
 //
 
-#define XSD_STR_VERSION "3.3.0.b2"
-#define XSD_INT_VERSION 3029952L
+#define XSD_STR_VERSION "3.3.0.b3"
+#define XSD_INT_VERSION 3029953L
 
 #endif  // XSD_CXX_VERSION_HXX
