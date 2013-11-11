b24970652f82ed60e3abb825e903beb506cfe814
Boris Kolpackov <boris@codesynthesis.com>
2013-11-11 09:37:45 +0200

    Drop support for VC 7.1

--- libxsd/xsd/cxx/post.hxx
+++ libxsd/xsd/cxx/post.hxx
@@ -3,11 +3,8 @@
 // copyright : Copyright (c) 2005-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2 + exceptions; see accompanying LICENSE file
 
-
 #ifdef _MSC_VER
 #  if (_MSC_VER >= 1400)
 #    include <xsd/cxx/compilers/vc-8/post.hxx>
-#  elif (_MSC_VER >= 1300)
-#    include <xsd/cxx/compilers/vc-7/post.hxx>
 #  endif
 #endif
--- libxsd/xsd/cxx/pre.hxx
+++ libxsd/xsd/cxx/pre.hxx
@@ -3,13 +3,10 @@
 // copyright : Copyright (c) 2005-2011 Code Synthesis Tools CC
 // license   : GNU GPL v2 + exceptions; see accompanying LICENSE file
 
-
 #ifdef _MSC_VER
 #  if (_MSC_VER >= 1400)
 #    include <xsd/cxx/compilers/vc-8/pre.hxx>
-#  elif (_MSC_VER >= 1300)
-#    include <xsd/cxx/compilers/vc-7/pre.hxx>
 #  else
-#    error Microsoft Visual C++ 6 is not supported.
+#    error Microsoft Visual C++ 7.1 and earlier are not supported
 #  endif
 #endif
